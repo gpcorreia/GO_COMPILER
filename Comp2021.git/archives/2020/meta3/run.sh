e48515f46e1388f037659818db69bc45d26b1e06
gpcorreia <goncalopcorreia.12@gmail.com>
2021-10-15 23:05:30 +0100

    new changes

--- Comp2021.git/archives/2020/meta3/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "{$1}"
-#./jucompiler -s < $1.java | diff $1.out -
-../3158 -s < $1.java | diff $1.out -
--- Comp2021.git/archives/2020b/meta1/illegal_char.c
+++
@@ -1,9 +0,0 @@
-int main(void){
-    int # = 223;
-    int var = 0;
-    if(var<1){
-        return 0;
-    }else{
-        return 1;
-    }
-}
